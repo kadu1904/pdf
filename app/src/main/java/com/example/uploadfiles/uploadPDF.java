039f8ecf137e855b64576cb5d3417bb7ee621084
Kadambari <k19patangrao@gmail.com>
2020-03-31 19:43:11 +0530

    pdf upload

---
+++ app/src/main/java/com/example/uploadfiles/uploadPDF.java
@@ -0,0 +1,31 @@
+package com.example.uploadfiles;
+
+public class uploadPDF {
+
+    public String name;
+    public String url;
+
+    public uploadPDF() {
+
+    }
+
+    public uploadPDF(String name, String url)
+    {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+
+
+    public String getUrl() {
+        return url;
+    }
+
+
+
+}
